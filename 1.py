675e03c2d8dd4ad943bb90acd7ed646790298d8c
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-02-20 16:07:31 +0530

    Initial commit

---
+++ 1.py
@@ -0,0 +1,15 @@
+def f(n):
+    if n==0 or n==1:
+        return n
+    else:
+        f0=0
+        f1=1
+        f=-1
+        for i in range(2,n+1):
+            f=f0+f1
+            f0=f1
+            f1=f
+        return f
+    
+n=int(input())
+print(f(n))
